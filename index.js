6781a2ce95341aff8b1c6e71ad2847e5f934873e
Leinad77ar <dxcordoba@outlook.com>
2023-03-01 19:18:34 -0300

    agregadas algunas funciones de js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 let navBarButtons = document.querySelectorAll(".navBarIcon ul li");
 let navBarIcon = document.querySelector(".navBarIcon");
-let arrow = document.getElementsByClassName("gototop");
-console.log(arrow)
-navBarButtons.forEach((link) =>{
- link.addEventListener("click", ()=> navBarIcon.classList.toggle("show"));
+let goTop = document.getElementsByClassName("gototop")[0];
+navBarButtons.forEach((link) => {
+    link.addEventListener("click", () => navBarIcon.classList.toggle("show"));
 })
+
+goTop.addEventListener("click", () => { scrollWindow() })
+let scrollWindow = () => {
+    window.scrollTo(0, 0);
+}
+window.onscroll = () => {
+    if (window.scrollY <= 900) {
+        goTop.style.display = "none"
+    }
+    else {
+        goTop.style.display = "block"
+    }
+}
+
